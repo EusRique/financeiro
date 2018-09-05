36ca2dcb78b2ee375a65c5184f75250e70c64174
Henrique <hiquesc_@hotmail.com>
2018-09-04 21:45:53 -0300

    Layout exibição saldo

--- config/adminlte.php
+++ config/adminlte.php
@@ -108,13 +108,27 @@ return [
     */
 
     'menu' => [
-        'MENU',
+        //'MENU',
         [
-            'text'        => 'Pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'      => 'Dashboard',
+            'url'       => 'admin',
+            'icon'      => 'dashboard',
+        ],
+        [
+            'text'      => 'Financeiro',
+            'icon'      => 'money',
+            'submenu'   => [
+                [
+                    'text'  => 'Saldo',
+                    'url'   => 'admin/balance',
+                    'icon'  => 'credit-card-alt'
+                ],
+                [
+                    'text'  => 'Histórico',
+                    'url'   => 'admin/historic',
+                    'icon'  => 'history'
+                ]
+            ]
         ]
     ],
 
