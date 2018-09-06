2a9660e9a742280c39ec7f0af9ac9794a4a778a4
Henrique <hiquesc_@hotmail.com>
2018-09-05 22:50:04 -0300

    Exibição de mensagens

--- app/Http/Controllers/Admin/BalanceController.php
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -25,6 +25,17 @@ class BalanceController extends Controller
     public function depositStore(MoneyValidation $request)
     {
         $balance = auth()->user()->balance()->firstOrCreate([]);
-        $balance->deposit($request->value);
+        $response = $balance->deposit($request->value);
+
+        if($response['success']) {
+
+            return redirect()
+                ->route('admin.balance')
+                ->with('success', $response['message']);
+        }
+
+        return redirect()
+            ->back()
+            ->with('error', $response['message']);
     }
 }
